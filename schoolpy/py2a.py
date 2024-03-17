24545672d35a41ab3d48cf00d8c8b6198d30c1ad
attilafekete73 <feketeattila2008@gmail.com>
2024-03-17 14:45:29 +0100

    delete

--- schoolpy/py2a.py
+++
@@ -1,8 +0,0 @@
-numa=int(input("Please enter a number! "))
-numb=int(input("Please enter another number! "))
-if numa > numb:
-    print(f'{numa} is greather than {numb}')
-elif numa<numb:
-    print(f'{numa} is less than {numb}')
-else:
-    print('The two numbers are equal')
--- schoolpy/py3b.py
+++
@@ -1,4 +0,0 @@
-sum=0
-for i in range(1,51):
-    sum=sum+(i*2)
-print(sum)
